188eb56dcc0ae97fae8e31e82fa11e666faa4dfb
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-10-12 06:46:20 +0530

    Added caption feature which can be added while creating posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_11_161730) do
+ActiveRecord::Schema.define(version: 2022_10_12_010203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2022_10_11_161730) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "active"
     t.bigint "account_id"
+    t.text "description"
     t.index ["account_id"], name: "index_posts_on_account_id"
   end
 
