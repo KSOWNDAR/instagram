4c3e50918fae72d808ccad3cf20ff523bd0c2666
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-10-12 21:33:48 +0530

    Suggestion Followers feature added

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,2 +1,7 @@
 module AccountsHelper
+
+    def profile_picture account, width, height 
+        image_path = account.profile_image.attached? ? account.profile_image : "default-profile.jpg" 
+        image_tag(image_path, width: width, height: height, :class => "profile-pic")
+    end
 end
