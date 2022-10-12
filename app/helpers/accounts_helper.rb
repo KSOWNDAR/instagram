ee0d427faf39ec997d1d4f28d12fb2813268ff8e
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-10-12 22:54:51 +0530

    Follow and follow back feature is added

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -4,4 +4,8 @@ module AccountsHelper
         image_path = account.profile_image.attached? ? account.profile_image : "default-profile.jpg" 
         image_tag(image_path, width: width, height: height, :class => "profile-pic")
     end
+
+    def can_edit_profile? profile_id
+        account_signed_in? && current_account.id == profile_id 
+    end
 end
