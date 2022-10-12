db9c21429b9ca481450c8559d22a7fc4220537d8
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-10-12 18:08:12 +0530

    Like Feature is added.Solved issue in jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+import "app/assets/javascripts/posts"
 //= require jquery3
 //= require popper
 //= require bootstrap
